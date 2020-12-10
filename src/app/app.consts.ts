3540ec1d23eb745ddd8e723df4c7529eb5dfbeed
Luiz Fernando Bicalho <luiz.bicalho@aglsolutions.com.br>
2020-12-10 18:56:04 -0300

    update

--- src/app/app.consts.ts
+++ src/app/app.consts.ts
@@ -1,10 +1,4 @@
-export const ApiEndpoint: string = "http://localhost/EVEN.AnaliseCredito.Web/";
+export const ApiEndpoint: string = "";
 export const Domain: string = "localhost";
 
 export const Ambiente: string = "Homologação";
-
-//export const DefaultHttpParameter = { UseCredentialOnSecureConnection: true, UseJWTOnSecureConnection: false };
-
-//export const DefaultNumberParameter = { allowNegative: true, decimalSize: 2, fixedDecimalSeparator: true, maxValue: 1000000, minValue: -1000000, showThousandSeparator: true };
-
-//export const GoogleKey = "AIzaSyBJAWevdl4KBUvMuoFj8KbX_3Ep8AmKBQs";
